1173e8867931ad6d873af10617e2f8301aa238c6
Isaac Frederick <sirisaacthefrederick@gmail.com>
2023-10-19 11:37:30 -0600

    does this fix anything? i hope so

---
+++ client/src/components/navbar.js
@@ -0,0 +1,37 @@
+import React from "react";
+// We import bootstrap to make our application look better.
+import "bootstrap/dist/css/bootstrap.css";
+ // We import NavLink to utilize the react router.
+import { NavLink } from "react-router-dom";
+ // Here, we display our Navbar
+export default function Navbar() {
+ return (
+   <div>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+       <NavLink className="navbar-brand" to="/">
+       <img style={{"width" : 100 + '%'}} src="https://pbs.twimg.com/ext_tw_video_thumb/1235208610696900617/pu/img/pWNiDSyFGu_bo_O2.jpg"></img>
+       </NavLink>
+       <button
+         className="navbar-toggler"
+         type="button"
+         data-toggle="collapse"
+         data-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent"
+         aria-expanded="false"
+         aria-label="Toggle navigation"
+       >
+         <span className="navbar-toggler-icon"></span>
+       </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+         <ul className="navbar-nav ml-auto">
+           <li className="nav-item">
+             <NavLink className="nav-link" to="/create">
+               Create Record
+             </NavLink>
+           </li>
+         </ul>
+       </div>
+     </nav>
+   </div>
+ );
+}
---
+++ client/src/components/recordList.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+const Record = (props) => (
+ <tr>
+   <td>{props.record.name}</td>
+   <td>{props.record.position}</td>
+   <td>{props.record.level}</td>
+   <td>
+     <Link className="btn btn-link" to={`/edit/${props.record._id}`}>Edit</Link> |
+     <button className="btn btn-link"
+       onClick={() => {
+         props.deleteRecord(props.record._id);
+       }}
+     >
+       Delete
+     </button>
+   </td>
+ </tr>
+);
+export default function RecordList() {
+ const [records, setRecords] = useState([]);
+  // This method fetches the records from the database.
+ useEffect(() => {
+  async function getRecords() {
+    try {
+      const response = await fetch(`http://localhost:5000/record/`, {
+        timeout: 10000, // 10 seconds
+      });
+  
+      if (!response.ok) {
+        const message = `An error occurred: ${response.statusText}`;
+        window.alert(message);
+        console.log("Something is wrong if it's coming here");
+        return;
+      }
+  
+      console.log("Now attempting to await response.json");
+      const records = await response.json();
+      console.log("Fetched records:", records);
+      setRecords(records);
+    } catch (error) {
+      console.error("Request timed out or failed:", error);
+      window.alert("Request timed out or failed. Please try again later.");
+    }
+  }
+  
+  getRecords();
+  return (
+    <div>
+      <h3>Record List</h3>
+      <table className="table table-striped" style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Level</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((record) => (
+            <Record
+              record={record}
+              deleteRecord={() => deleteRecord(record._id)} // You need to define deleteRecord function
+              key={record._id}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+ }, []);
+  // This method will delete a record
+ async function deleteRecord(id) {
+   await fetch(`http://localhost:5000/${id}`, {
+     method: "DELETE"
+   });
+    const newRecords = records.filter((el) => el._id !== id);
+   setRecords(newRecords);
+ }
+  // This method will map out the records on the table
+ function recordList() {
+   return records.map((record) => {
+     return (
+       <Record
+         record={record}
+         deleteRecord={() => deleteRecord(record._id)}
+         key={record._id}
+       />
+     );
+   });
+ }
+  // This following section will display the table with the records of individuals.
+ return (
+   <div>
+     <h3>Record List</h3>
+     <table className="table table-striped" style={{ marginTop: 20 }}>
+       <thead>
+         <tr>
+           <th>Name</th>
+           <th>Position</th>
+           <th>Level</th>
+           <th>Action</th>
+         </tr>
+       </thead>
+       <tbody>{recordList()}</tbody>
+     </table>
+   </div>
+ );
+}
